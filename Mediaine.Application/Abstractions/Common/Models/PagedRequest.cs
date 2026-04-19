98bfe119dba88cad4b353415ef93d8b4f73e71c0
Aditya Pratama Febriono <adityapeef18@gmail.com>
2026-04-19 17:03:19 +0700

    refactor PagedRequest

--- Mediaine.Application/Abstractions/Common/Models/PagedRequest.cs
+++ Mediaine.Application/Abstractions/Common/Models/PagedRequest.cs
@@ -2,7 +2,20 @@ namespace Mediaine.Application.Common.Models;
 
 public class PagedRequest
 {
-    public int Page { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
+    private int _page = 1;
+    private int _pageSize = 10;
+
+    public int Page
+    {
+        get => _page;
+        set => _page = value > 0 ? value : 1;
+    }
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > 0 ? value : 10;
+    }
+
     public string? Search { get; set; }
 }
